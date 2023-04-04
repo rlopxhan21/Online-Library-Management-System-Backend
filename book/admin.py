9c59d087e9ae219366063dc892c04d2fd6173990
rlopxhan21 <rlopxhan21@gmail.com>
2023-04-04 14:48:17 +1000

    update admin view:

--- book/admin.py
+++ book/admin.py
@@ -5,7 +5,7 @@ from .models import Author, Genre, Book
 admin.site.register(Genre)
 admin.site.register(Author)
 
-@admin.site.resgister(Book)
+@admin.site.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ['name', 'author', 'status', 'genre', 'patrons', "end_at"]
     prepopulated_fields= {"slug": ('title',)}
